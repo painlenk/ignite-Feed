086d1630dfa5d455211b68ea4015e95d79576191
leonardo valle <leonardo.valle@eduzz.com>
2022-11-07 20:54:10 -0300

    cria e estiliza textos e comentarios

---
+++ src/Components/Divider/index.tsx
@@ -0,0 +1,11 @@
+export const Divider = () => {
+  return (
+    <div
+      style={{
+        borderTop: "1px solid #323238",
+        width: "100%",
+        margin: "24px 0",
+      }}
+    ></div>
+  );
+};
--- src/Components/Post/index.tsx
+++ src/Components/Post/index.tsx
@@ -1,3 +1,4 @@
+import { Divider } from "../Divider";
 import styles from "./styles.module.css";
 
 export const Post = () => {
@@ -17,7 +18,37 @@ export const Post = () => {
           </div>
         </div>
 
-        <span className={styles.publicated_At}>Públicado há 1h</span>
+        <time
+          title="11 de maio as 08:13h"
+          dateTime="2022-05-11 08:13:30"
+          className={styles.publicated_At}
+        >
+          Públicado há 1h
+        </time>
+      </div>
+
+      <div className={styles.content_Container}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href=""> jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+
+      <Divider />
+
+      <div className={styles.comment_Container}>
+        <span>Deixe seu feedback</span>
+
+        <textarea className={styles.comment_Area} />
+
+        <div className={styles.publish_Comment}>Publicar</div>
       </div>
     </section>
   );
